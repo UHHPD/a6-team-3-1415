94aa41f2f0c246da5d3180bfe04d04af65068608
Constanttp <82391985+Constant-t-p@users.noreply.github.com>
2021-06-14 15:47:24 +0000

    Blatt 6 fertig?

--- Vollzylinder.cc
+++ Vollzylinder.cc
@@ -9,11 +9,12 @@ Vollzylinder::Vollzylinder(double nr, double nl) {
 }
 
 Vektor Vollzylinder::punkt() {
-  double R = 0;
+  double R = r_;
   double phi = 2 * M_PI * rand() / (double)RAND_MAX;
   double z = l_ * (rand() / (double)RAND_MAX - 0.5);
+  double r = R*sqrt(rand()/(double)RAND_MAX); 
 
-  double x = 0;
-  double y = 0;
+  double x = r*cos(phi);
+  double y = r*sin(phi);
   return Vektor(x, y, z);
 };
--- traegheit.cc
+++ traegheit.cc
@@ -7,7 +7,7 @@
 int main() {
   const int N = 10000;     // Anzahl Integrationspunkte
   const double M = 1;      // Masse des Zylindermantels
-  const double ZM_R = 1.0; // Radius der Zylindermantels
+  const double ZM_R = 2.0; // Radius der Zylindermantels
   const double ZM_L = 1.0; // Laenge des Zylindermantels
 
   Vektor a; // Punkt auf der Rotationsachse
@@ -23,14 +23,32 @@ int main() {
   double m = M / N; // Masse eines Massenpunktes
   for (int i = 0; i < N; ++i) {
     Vektor x = zm->punkt();
+    Vektor c = x - a;
+    Vektor d = c.kreuz(u);
     // Abstand Punkt x und Gerade a + t*u
     // Vektor n = ...;//Normalenvektor x-a kreuz u
-    double r = 0; //|n|/|u|
+    double r = d.betrag()/u.betrag(); //|n|/|u|
     // std::cout << x << " :" << r << std::endl;
     // addiere Beitrag des Massenpunktes zum Traegheitsmoment
     J += m * r * r;
   }
   std::cout << "Massentraegheitsmoment fuer einen Zylindermantel"
+            << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
+  
+  std::unique_ptr<Vollzylinder> vz(new Vollzylinder(ZM_R, ZM_L));
+
+  for (int i = 0; i < N; ++i) {
+    Vektor x = vz->punkt();
+    Vektor c = x - a;
+    Vektor d = c.kreuz(u);
+    // Abstand Punkt x und Gerade a + t*u
+    // Vektor n = ...;//Normalenvektor x-a kreuz u
+    double r = d.betrag()/u.betrag(); //|n|/|u|
+    // std::cout << x << " :" << r << std::endl;
+    // addiere Beitrag des Massenpunktes zum Traegheitsmoment
+    J += m * r * r;
+  }
+  std::cout << "Massentraegheitsmoment fuer einen Vollzylinder"
             << " mit a = " << a << " und u = " << u << ": " << J << std::endl;
             
   return 0;
